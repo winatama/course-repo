e4fa07f2326291b9054dc2313d8e7673355d43da
winatama <winatamafza@gmail.com>
2024-02-28 12:21:20 +0700

    update progres

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+(() => {
+    const btnOpen = document.getElementById('btn-open');
+    const btnClose= document.getElementById('btn-close');
+    const toggle = document.getElementById('toggle');
+    const openbar = () => {
+        toggle.style.display = 'block';
+    }
+    const closebar = () => {
+        toggle.style.display = 'none';
+    }
+    btnOpen.addEventListener('click', openbar);
+    btnClose.addEventListener('click', closebar);
+})();
